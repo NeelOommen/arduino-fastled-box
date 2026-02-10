a514eddea47578476cb76dd182c0be79832a95ed
NeelOommen <70756241+NeelOommen@users.noreply.github.com>
2026-02-10 22:22:45 +0530

    Added basic FFT

--- arduinoFastLedBox/arduinoFastLedBox.ino
+++ arduinoFastLedBox/arduinoFastLedBox.ino
@@ -1,8 +1,13 @@
 #include <FastLED.h>
+#include <arduinoFFT.h>
+
+#define FFT_SIZE 64
+#define SAMPLE_RATE 5000   // Hz
+#define SAMPLE_INTERVAL_US (1000000 / SAMPLE_RATE)
 
 #define LED_PIN     5
 #define NUM_LEDS    64
-#define BRIGHTNESS  32
+#define BRIGHTNESS  8
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
@@ -14,17 +19,34 @@ CRGB colorMap[NUM_LEDS];
 #define SENSOR_MIN 0
 #define DEAD_ZONE 120
 
+enum VisualizationMode {
+  VIS_AMPLITUDE,
+  VIS_FFT
+};
+
+VisualizationMode currentMode = VIS_FFT;
+
 int sensorPin = A1; 
 int sensorValue = 0;
 
 int filteredValue = 0;
 
 int display[8];
+int targetDisplay[8];
 
 uint32_t lastFrameTime = 0;
 const uint32_t FRAME_INTERVAL = 1000 / UPDATES_PER_SECOND;
 
 int framePeak = 0;
+
+double vReal[FFT_SIZE];
+double vImag[FFT_SIZE];
+
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, FFT_SIZE, SAMPLE_RATE);
+
+uint16_t sampleIndex = 0;
+uint32_t lastSampleMicros = 0;
+bool fftReady = false;
 
 void fillRainbowPalette();
 
@@ -32,30 +54,80 @@ void setup() {
   delay( 3000 ); // power-up safety delay
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
-  // Serial.begin(9600);
   fillRainbowPalette(0);
 }
 
 void loop() {
   sensorValue = analogRead(sensorPin);
-  int ledCount = mapSensorReadToLEDCount(sensorValue, 18);
- 
-  ledCount = smooth(ledCount);
 
+  if (currentMode == VIS_FFT) {
+  uint32_t now = micros();
+
+    if (now - lastSampleMicros >= SAMPLE_INTERVAL_US) {
+      lastSampleMicros += SAMPLE_INTERVAL_US;
+
+      vReal[sampleIndex] = analogRead(sensorPin) - 512;
+      vImag[sampleIndex] = 0;
+
+      sampleIndex++;
+
+      if (sampleIndex >= FFT_SIZE) {
+        sampleIndex = 0;
+        fftReady = true;
+      }
+    }
+  }
+
+  updateVisualization();
+
+  if (frameReady()) {
+
+    if (currentMode == VIS_AMPLITUDE) {
+      shiftInNewValueToDisp(framePeak);
+    }
+
+    if (currentMode == VIS_FFT) {
+      applySmoothingAndDecay();
+    }
+
+    drawVisualizer();
+  }
+}
+
+void updateVisualization() {
+  switch (currentMode) {
+    case VIS_AMPLITUDE:
+      updateAmplitude();
+      break;
+
+    case VIS_FFT:
+      updateFFT();
+      break;
+  }
+
+
+}
+
+void updateAmplitude() {
+  int ledCount = smooth(mapSensorReadToLEDCount(sensorValue, 20));
   if (ledCount > framePeak){
     framePeak = ledCount;
   }
+}
 
-  uint32_t now = millis();
-
-  if (now - lastFrameTime >= FRAME_INTERVAL) {
-    lastFrameTime = now;
-
-    shiftInNewValueToDisp(framePeak);
-    drawVisualizer();
-
-    framePeak = 0;
+void updateFFT() {
+  if (!fftReady){
+    return;
   }
+
+  fftReady = false;
+
+  FFT.windowing(vReal, FFT_SIZE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+
+  FFT.compute(vReal, vImag, FFT_SIZE, FFT_FORWARD);
+  FFT.complexToMagnitude(vReal, vImag, FFT_SIZE);
+
+  computeFFTBands();
 }
 
 int mapSensorReadToLEDCount(int sensorValue, int maxVal) {
@@ -75,10 +147,52 @@ int mapSensorReadToLEDCount(int sensorValue, int maxVal) {
     return (curved * maxVal) / 1024;
 }
 
-void fillLeds(int numLedsToFill){
-  clearLeds();
-  for(int i = 0; i<numLedsToFill; i++){
-    leds[i] = CRGB::Red;
+void computeFFTBands() {
+
+  uint8_t bandLimits[9] = {
+    2, 3, 5, 7, 10, 14, 20, 26, 31
+  };
+
+  for (int band = 0; band < 8; band++) {
+
+    double sum = 0;
+
+    for (int i = bandLimits[band];
+         i < bandLimits[band + 1];
+         i++) {
+      sum += vReal[i];
+    }
+
+    int value = sum / (bandLimits[band + 1] - bandLimits[band]);
+
+    // simple scaling (we'll improve later)
+    value = value / 20;
+
+    if (value > 8) value = 8;
+
+    targetDisplay[band] = (targetDisplay[band] * 3 + value) / 4;
+  }
+}
+
+void applySmoothingAndDecay() {
+
+  const int riseSpeed = 3;  // fast rise
+  const int fallSpeed = 1;  // slow decay
+
+  for (int i = 0; i < 8; i++) {
+
+    if (targetDisplay[i] > display[i]) {
+      // rise quickly
+      display[i] += riseSpeed;
+      if (display[i] > targetDisplay[i])
+        display[i] = targetDisplay[i];
+    }
+    else if (targetDisplay[i] < display[i]) {
+      // decay slowly
+      display[i] -= fallSpeed;
+      if (display[i] < 0)
+        display[i] = 0;
+    }
   }
 }
 
@@ -92,6 +206,9 @@ int smooth(int newValue) {
     filteredValue = (filteredValue * 7 + newValue) / 8;
     return filteredValue;
 }
+
+
+/* VISUALIZATION FUNCTIONS */
 
 void drawVisualizer(){
   clearLeds();
@@ -117,7 +234,19 @@ void shiftInNewValueToDisp(int newValue){
     display[i] = display[i + 1];
   }
   display[7] = newValue;
+  framePeak = 0;
 }
+
+bool frameReady(){
+  uint32_t now = millis();
+  if(now - lastFrameTime >= FRAME_INTERVAL){
+    lastFrameTime = now;
+    return true;
+  }
+  return false;
+}
+
+/*PALETTES*/
 
 void fillRainbowPalette(uint8_t colorIndex){
   uint8_t brightness = 255;
